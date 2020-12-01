7777d0d84d21dcb476f46ae6ccc1db77e5cf501f
ScrubbyAlien <samuel.eskilson@gmail.com>
2020-12-01 15:04:37 +0100

    dag ett är klar

---
+++ 2020/01.py
@@ -0,0 +1,30 @@
+expenses_list = list(
+    map(int, open("2020/inputs/input01.txt", "r").readlines())
+)
+
+# part 1
+
+a = 0
+b = 0
+
+for i in expenses_list:
+    for j in expenses_list:
+        if(i + j == 2020):
+            a = i
+            b = j
+
+print(a * b)
+
+# part 2
+
+c = 0
+
+for i in expenses_list:
+    for j in expenses_list:
+        for k in expenses_list:
+            if(i + j + k == 2020):
+                a = i
+                b = j
+                c = k
+
+print(a * b * c)
