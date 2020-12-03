3fdc0819dacee6d3356a5836d82bf98d79551db8
ScrubbyAlien <samuel.eskilson@gmail.com>
2020-12-03 17:56:18 +0100

    03 klart

--- 2020/03.py
+++ 2020/03.py
@@ -1,2 +1,44 @@
 grid = open("2020/inputs/input03.txt", "r").readlines()
-starting_pos = (1, 1)
+
+
+# part 1
+
+pos = 0
+trees = 0
+
+print(len(grid))
+
+width = len(grid[0]) - 1
+
+for i in range(1, len(grid)):
+    row = grid[i].rstrip("\n")
+    pos += 3
+    pos %= width
+
+    if (row[pos] == "#"):
+        trees += 1
+print(trees)
+
+
+# part 2
+
+trees = 0
+
+tuples = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+product = 1
+print(len(grid))
+for x, y in tuples:
+    pos = 0
+    for i in range(0, len(grid), y):
+        row = grid[i].rstrip("\n")
+        if (row[pos] == "#"):
+            trees += 1
+        pos = (pos+x) % width
+    product *= trees
+    trees = 0
+print(product)
+
+
+# 360502272
+# 4352292000
+# 3695737500
