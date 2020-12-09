8cb16868fa5c1e251cfcc8cc3eb37a88184b0a20
ScrubbyAlien <samuel.eskilson@gmail.com>
2020-12-09 18:28:50 +0100

    del ett klar del två nästan

---
+++ 2020/09.py
@@ -0,0 +1,32 @@
+import time
+
+numbers = list(map(int, open("2020/inputs/input09.txt", "r").readlines()))
+
+# part 1
+
+wrong_number = 0
+
+for i in range(25, len(numbers)):
+    possible_numbers = []
+    for x in range(i-25, i):
+        for y in range(i-25, i):
+            if(numbers[x] != numbers[y]):
+                possible_numbers.append(numbers[x] + numbers[y])
+    if(numbers[i] not in possible_numbers):
+        print(numbers[i])
+        wrong_number = numbers[i]
+
+
+# part 2
+
+start = time.time()
+
+for r in range(2, len(numbers) + 1):
+    for i in range(0, len(numbers)-r+1):
+        if(sum(numbers[i:i+r-1]) == wrong_number):
+            print(numbers[i] + numbers[i + r-1])
+            break
+
+end = time.time()
+
+print(end - start)
