e44f7936aec75f5773011cf17c7af44e2197d995
Samuel Eskilson <samuel.eskilson@gmail.com>
2021-11-29 22:05:40 +0100

    2019 day 1 part 1 klar!

--- aoc2019/src/aoc01.rs
+++ aoc2019/src/aoc01.rs
@@ -1,3 +1,19 @@
+use std::fs;
+
+
 pub fn foobar() {
-    println!("Hej!");
+    let filename = "inputs/input01.txt";
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let list: Vec<_> = contents.trim().split('\n').collect();
+
+    let mut sum = 0;
+
+    for elem in list {
+        let num: f32 = elem.parse().unwrap();
+
+        sum = sum + (num/3.0).floor() as i32 - 2;
+    }
+
+    println!("{}", sum);
 }
